696a60ea4ccb14e650453c3da2c3257683e62c1b
keeeei13c <kei.121003f@icloud.com>
2022-02-14 14:56:18 +0900

    ✨ タスク管理のUI作成 #2

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,17 +1,93 @@
 import type { VFC } from "react";
-import { Button } from "src/component/Button";
+import { useState } from "react";
 
-export const Index: VFC = () => {
-  const handleClick = () => {
-    window.alert("Hello, World!");
+const todayList: string[] = ["Qin Todo", "A simple todo app", "by Qin", "hoge"];
+const tomorrowList: string[] = ["Qin Todo", "A simple todo app", "by Qin"];
+const nextList: string[] = ["Qin Todo", "A simple todo app", "by Qin"];
+
+export type Props = {
+  label: string;
+  color: string;
+  backgroundColor: string;
+  todoList?: string[];
+};
+
+type DeadLine = {
+  todayList: string[];
+  tomorrowList: string[];
+  nextList: string[];
+};
+
+const classNames = (...classes: string[]) => classes.filter(Boolean).join(" ");
+
+const Todo = ({ backgroundColor, color, label, todoList }: Props) => {
+  const [isFinished, setIsFinished] = useState<boolean>(false);
+
+  const handleIsFinished = () => {
+    setIsFinished((prev) => !prev);
   };
 
   return (
-    <div>
-      <h2>Index</h2>
-      <Button tag="button" className="p-2" onClick={handleClick}>
-        Click me!
-      </Button>
-    </div>
+    <>
+      <div className="mb-8">
+        <h1 className={`p-2 text-xl ${color}`}>{label}</h1>
+
+        {todoList &&
+          todoList.map((item, index) => (
+            <div key={index} className="flex items-center">
+              <button
+                title="taskButton"
+                className={classNames(
+                  "p-2 m-2 border-2 border-gray-200 rounded-full",
+                  isFinished ? backgroundColor : "bg-white"
+                )}
+                onClick={handleIsFinished}
+              />
+              <div
+                className={
+                  isFinished ? "text-gray-500 line-through" : "text-base"
+                }
+              >
+                {item}
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
+
+export const TodoIndex = ({ nextList, todayList, tomorrowList }: DeadLine) => (
+  <>
+    <div className="py-2 px-4">
+      <Todo
+        color="text-rose-500"
+        backgroundColor="bg-rose-500"
+        label="今日する"
+        todoList={todayList}
+      />
+      <Todo
+        color="text-orange-400"
+        backgroundColor="bg-orange-400"
+        label="明日する"
+        todoList={tomorrowList}
+      />
+      <Todo
+        color="text-amber-400"
+        backgroundColor="bg-amber-400"
+        label="今度する"
+        todoList={nextList}
+      />
+    </div>
+  </>
+);
+
+export const Index: VFC = () => (
+  <>
+    <TodoIndex
+      todayList={todayList}
+      tomorrowList={tomorrowList}
+      nextList={nextList}
+    />
+  </>
+);
