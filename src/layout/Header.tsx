9e1496eccf070f28cf932c0365c6edfc5b872312
cti1650 <cti1650@gmail.com>
2022-02-15 23:36:10 +0900

    feat:QinLogo追加

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,4 +1,5 @@
 import type { VFC } from "react";
+import { QinLogo } from "src/component/Logo/QinLogo";
 
 /**
  * @package
@@ -9,7 +10,9 @@ export const Header: VFC = () => (
     <nav className="p-4 bg-white ">
       <div className="flex flex-row justify-between">
         <div></div>
-        <div className="text-xl text-center">Qin Todo</div>
+        <div className="text-xl text-center">
+          <QinLogo />
+        </div>
         <div className="mr-0 w-8 h-8 bg-blue-300 rounded-full"></div>
       </div>
     </nav>
