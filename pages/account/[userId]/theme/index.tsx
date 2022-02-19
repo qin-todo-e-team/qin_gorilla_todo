fc4228ec5ff82d72edf9f59ce8b0ddfc045b8892
yugo-ibuki <y.ibuki91@gmail.com>
2022-02-19 22:54:30 +0900

    refactor: Layoutをcomponentに移動

--- pages/account/[userId]/theme/index.tsx
+++ pages/account/[userId]/theme/index.tsx
@@ -1,7 +1,6 @@
+import { SettingLayout } from "@component/Layout/setting";
 import { Theme } from "@component/theme";
 import type { NextPage } from "next";
-
-import { SettingLayout } from "../../../layout/setting";
 
 const Index: NextPage = () => (
   <SettingLayout title={"テーマ"}>
