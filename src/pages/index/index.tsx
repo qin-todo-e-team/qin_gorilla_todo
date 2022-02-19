f16261fd0ed4d185a5e74fcdfe98c43f76b76607
katayama <t_katayama@tfusion.co.jp>
2022-02-19 14:31:32 +0900

    feat: firebaseのデータを表示 #32

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,30 @@
+import { collection, onSnapshot, query } from "firebase/firestore";
 import type { VFC } from "react";
+import { useEffect, useState } from "react";
 import { TodoList } from "src/component/TodoList/TodoList";
 
-const todayList: string[] = ["Qin Todo", "A simple todo app", "by Qin", "hoge"];
+import { db } from "../../../config/firebase";
+
+//const todayList: string[] = ["Qin Todo", "A simple todo app", "by Qin", "hoge"];
 const tomorrowList: string[] = ["Qin Todo", "A simple todo app", "by Qin"];
 const nextList: string[] = ["Qin Todo", "A simple todo app", "by Qin"];
 
-export const Index: VFC = () => (
-  <>
-    <TodoList
-      todayList={todayList}
-      tomorrowList={tomorrowList}
-      nextList={nextList}
-    />
-  </>
-);
+export const Index: VFC = () => {
+  const [todayList, setTodayList] = useState<string[]>([]);
+  useEffect(() => {
+    const q = query(collection(db, "Todos"));
+    onSnapshot(q, (querySnapshot) => {
+      setTodayList(querySnapshot.docs.map((data) => data.data().name));
+    });
+  }, []);
+
+  return (
+    <>
+      <TodoList
+        todayList={todayList}
+        tomorrowList={tomorrowList}
+        nextList={nextList}
+      />
+    </>
+  );
+};
