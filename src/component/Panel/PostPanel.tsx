8b3156b2fddfb7915c7f6b43a1bc2675dde3a7bd
cti1650 <cti1650@gmail.com>
2022-02-26 18:05:28 +0900

    feat: Taskをfirebaseに追加する機能の検証

--- src/component/Panel/PostPanel.tsx
+++ src/component/Panel/PostPanel.tsx
@@ -1,4 +1,6 @@
+import { db } from "@config/firebase";
 import cc from "classcat";
+import { addDoc, collection } from "firebase/firestore";
 import React, { useState } from "react";
 
 import { PostButton } from "./PostButton";
@@ -15,6 +17,41 @@ type PostPanelPropsType = {
   value?: string;
 };
 
+type PostDataType = {
+  expireDate: Date;
+  name: string;
+  isDeleted: boolean;
+  isFinished: boolean;
+};
+
+// TODO: カスタムフックに分離するか検討中
+const postTaskData = async (expire: string, value: string) => {
+  const getExpireDate = (expire: string) => {
+    const now = new Date();
+    const tomorrow = new Date(now);
+    tomorrow.setDate(now.getDate() + 1);
+    const upcoming = new Date(now);
+    upcoming.setDate(now.getDate() + 9999); // TODO: 仮で9999日後に設定
+    switch (expire) {
+      case "today":
+        return now;
+      case "tomorrow":
+        return tomorrow;
+      case "upcoming":
+        return upcoming;
+      default:
+        return now;
+    }
+  };
+  const data: PostDataType = {
+    expireDate: getExpireDate(expire),
+    name: value ?? "",
+    isDeleted: false,
+    isFinished: false,
+  };
+  await addDoc(collection(db, "Todos"), data);
+};
+
 export const PostPanel: React.VFC<PostPanelPropsType> = ({
   onChange,
   onSubmit, // TODO: テキストボックスに入力された値を返すイベント（不要であれば削除します）
@@ -28,12 +65,13 @@ export const PostPanel: React.VFC<PostPanelPropsType> = ({
     onChange && onChange(textboxValue);
   };
 
-  const handleSubmit = (typeString: string) => {
+  const handleSubmit = async (typeString: string) => {
     onSubmit &&
       onSubmit({
         type: typeString || "",
         value: textboxValue || "",
       });
+    postTaskData(typeString, textboxValue);
     setTextboxValue("");
   };
   return (
