295c67e7d01c2b0e83e36883152ea96e2d04e0f1
katayama <t_katayama@tfusion.co.jp>
2022-02-26 15:50:29 +0900

    feat: リアルタイム表示

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,14 +1,38 @@
 import { PostPanel } from "@component/Panel/PostPanel";
 import { TodoList } from "@component/TodoList/TodoList";
+import type {
+  DocumentData,
+  FirestoreError,
+  QuerySnapshot,
+} from "firebase/firestore";
+import { collectionGroup } from "firebase/firestore";
 import type { VFC } from "react";
 import React, { useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
 
-const todayList: string[] = ["Qin Todo", "A simple todo app", "by Qin", "hoge"];
-const tomorrowList: string[] = ["Qin Todo", "A simple todo app", "by Qin"];
+import { db } from "../../../config/firebase";
+
+export const FirestoreCollection = (
+  Todos: string
+): {
+  value: QuerySnapshot<DocumentData> | undefined;
+  loading: boolean;
+  error: FirestoreError | undefined;
+} => {
+  const [value, loading, error] = useCollection(collectionGroup(db, Todos), {
+    snapshotListenOptions: { includeMetadataChanges: true },
+  });
+  return { value, loading, error };
+};
+
 const nextList: string[] = ["Qin Todo", "A simple todo app", "by Qin"];
 
 export const Index: VFC = () => {
   const [nextToDoList, setNextToDoList] = useState(nextList);
+  const { value } = FirestoreCollection("Todos");
+  const todayList = value?.docs.map((doc) => doc.data().name) ?? [];
+  const tomorrowList = value?.docs.map((doc) => doc.data().name) ?? [];
+
   return (
     <>
       <TodoList
