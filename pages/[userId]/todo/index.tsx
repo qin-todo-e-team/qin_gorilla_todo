878951d9c08a84b7ef2d84f608f90cf01fabdbf8
cti1650 <cti1650@gmail.com>
2022-03-06 17:33:21 +0900

    feat: Todoページ作成 #47 (#51)

---
+++ pages/[userId]/todo/index.tsx
@@ -0,0 +1,6 @@
+import type { CustomNextPage } from "next";
+import { Index } from "src/pages/[userId]/todo";
+
+const IndexPage: CustomNextPage = () => <Index />;
+
+export default IndexPage;
---
+++ src/pages/[userId]/todo/index.tsx
@@ -0,0 +1,21 @@
+import { PostPanel } from "@component/Panel/PostPanel";
+import { TodoList } from "@component/TodoList/TodoList";
+import React, { useState } from "react";
+import { Todos } from "src/mock";
+
+const nextList: string[] = ["Qin Todo", "A simple todo app", "by Qin"];
+
+export const Index: React.VFC = () => {
+  // TODO: nextToDoListはfirebaseと連携出来次第削除します。
+  const [nextToDoList, setNextToDoList] = useState(nextList);
+  return (
+    <>
+      <TodoList todayList={Todos} tomorrowList={Todos} upcomingList={Todos} />
+      <PostPanel
+        onSubmit={(data) => {
+          setNextToDoList((prev) => [...prev, data.value]);
+        }}
+      />
+    </>
+  );
+};
