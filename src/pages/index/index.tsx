6be119ecbc62a8bd636735e1530cda8c6f8cf664
keeeei13c <kei.121003f@icloud.com>
2022-03-07 00:57:04 +0900

    コンポーネント改善に伴う変更

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,17 +1,11 @@
 import { PostPanel } from "@component/Panel/PostPanel";
 import { TodoList } from "@component/TodoList/TodoList";
 import type { VFC } from "react";
-import React, { useState } from "react";
 import { Todos } from "src/mock";
 
-const nextList: string[] = ["Qin Todo", "A simple todo app", "by Qin"];
-
-export const Index: VFC = () => {
-  const [nextToDoList, setNextToDoList] = useState(nextList);
-  return (
-    <>
-      <TodoList todayList={Todos} tomorrowList={Todos} upcomingList={Todos} />
-      <PostPanel />
-    </>
-  );
-};
+export const Index: VFC = () => (
+  <>
+    <TodoList todoList={Todos} />
+    <PostPanel />
+  </>
+);
