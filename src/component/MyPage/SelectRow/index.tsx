f228ec146c261a93ca78ad3f6d5180a764dc747c
MABO-0917 <mkohei14@gmail.com>
2022-03-07 13:34:11 +0900

    feat: マイページにおける遷移機能実装

--- src/component/MyPage/SelectRow/index.tsx
+++ src/component/MyPage/SelectRow/index.tsx
@@ -1,14 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {
   children: React.ReactNode;
+  url: string;
 };
 
-export const SelectRow: React.VFC<Props> = ({ children }) => (
+export const SelectRow: React.VFC<Props> = ({ children, url }) => (
   <li>
-    <button className="flex justify-between w-full">
-      <div className="py-2 font-bold text-left">{children}</div>
-      <div className="py-2 font-bold text-right text-gray-300">&gt;</div>
-    </button>
+    <Link href={url}>
+      <a className="flex justify-between w-full">
+        <div className="py-2 font-bold text-left">{children}</div>
+        <div className="py-2 font-bold text-right text-gray-300">&gt;</div>
+      </a>
+    </Link>
   </li>
 );
