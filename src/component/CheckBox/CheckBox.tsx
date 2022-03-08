bcadf1369c53643e10decea11b8bfd3ffce1c3c7
keeeei13c <67566483+keeeei13c@users.noreply.github.com>
2022-03-08 21:59:24 +0900

    Refactor/checkbox (#58)
    * コンポーネントを改善
    
    * コンポーネント改善に伴う変更
    
    * Listの名前変更

--- src/component/CheckBox/CheckBox.tsx
+++ src/component/CheckBox/CheckBox.tsx
@@ -1,49 +1,78 @@
 import { RadioButton } from "@component/RadioButton";
+import cc from "classcat";
 import { useState } from "react";
 import type { Todo } from "src/models/Todo";
 
 type Props = {
-  label: string;
-  color: string;
-  backgroundColor: string;
+  title: "today" | "upcoming" | "tomorrow" | undefined;
   todoList: Todo[];
+  children?: React.ReactNode;
 };
 
-export const CheckBox = ({
-  backgroundColor,
-  color,
-  label,
-  todoList,
-}: Props) => {
-  const [isFinished, setIsFinished] = useState<boolean[]>(
+type ListItem = {
+  label: string;
+  color: string;
+  bg: string;
+};
+
+type ThemeListType = {
+  today: ListItem;
+  tomorrow: ListItem;
+  upcoming: ListItem;
+};
+
+const ThemeLists: ThemeListType = {
+  today: {
+    label: "今日する",
+    color: "text-rose-500",
+    bg: "bg-rose-500",
+  },
+  tomorrow: {
+    label: "明日する",
+    color: "text-orange-400",
+    bg: "bg-orange-400",
+  },
+  upcoming: {
+    label: "今度する",
+    color: "bg-amber-400",
+    bg: "bg-amber-400",
+  },
+};
+
+export const CheckBox = ({ todoList, title }: Props) => {
+  const [isFinishedList, setIsFinishedList] = useState<boolean[]>(
     todoList.map((todo) => todo.isFinished)
   );
-  const handleFinished = (i: number) => {
-    const newArray: boolean[] = isFinished.map((flag, index) =>
-      i === index ? !flag : flag
+  const handleFinished = (index: number) => {
+    const newArray: boolean[] = isFinishedList.map((flag, i) =>
+      index === i ? !flag : flag
     );
-    setIsFinished(newArray);
+    setIsFinishedList(newArray);
   };
+
+  const { label, color, bg } = ThemeLists[title ?? "today"];
 
   return (
     <>
       <div className="mb-8">
-        <h1 className={`p-2 text-xl font-bold ${color}`}>{label}</h1>
+        <h1 className={cc(["p-2 text-xl font-bold", color])}>{label}</h1>
         <div>
           <div className="flex flex-col">
             {todoList.map((todo, index) => (
-              <div key={index} className="flex items-center p-2">
+              <div key={index} className="flex items-center p-2 ">
                 <RadioButton
-                  color={backgroundColor}
-                  isSelected={isFinished[index]}
+                  color={bg}
+                  isSelected={isFinishedList[index]}
                   handleSelected={() => handleFinished(index)}
                 />
                 <div
-                  className={`ml-2 text-lg ${
-                    isFinished[index]
-                      ? "text-gray-500 line-through"
-                      : "text-base"
-                  }`}
+                  className={cc([
+                    "ml-2 text-lg",
+                    {
+                      "text-gray-500 line-through ":
+                        isFinishedList[index] === true,
+                    },
+                  ])}
                 >
                   {todo.name}
                 </div>
--- src/component/TodoList/TodoList.tsx
+++ src/component/TodoList/TodoList.tsx
@@ -2,32 +2,13 @@ import { CheckBox } from "src/component/CheckBox/CheckBox";
 import type { Todo } from "src/models/Todo";
 
 type Props = {
-  todayList: Todo[];
-  tomorrowList: Todo[];
-  upcomingList: Todo[];
+  todoList: Todo[];
 };
 
-export const TodoList = ({ todayList, tomorrowList, upcomingList }: Props) => (
+export const TodoList = ({ todoList }: Props) => (
   <>
     <div className="py-2 px-4">
-      <CheckBox
-        color="text-rose-500"
-        backgroundColor="bg-rose-500"
-        label="今日する"
-        todoList={todayList}
-      />
-      <CheckBox
-        color="text-orange-400"
-        backgroundColor="bg-orange-400"
-        label="明日する"
-        todoList={tomorrowList}
-      />
-      <CheckBox
-        color="text-amber-400"
-        backgroundColor="bg-amber-400"
-        label="今度する"
-        todoList={upcomingList}
-      />
+      <CheckBox todoList={todoList} title="today" />
     </div>
   </>
 );
