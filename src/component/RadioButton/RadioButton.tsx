4b53b094736822dee13721d820df7834afc9ebdf
keeeei13c <kei.121003f@icloud.com>
2022-03-09 09:28:47 +0900

    prettier

--- src/component/RadioButton/RadioButton.tsx
+++ src/component/RadioButton/RadioButton.tsx
@@ -9,7 +9,7 @@ export const RadioButton = ({ color, handleSelected, isSelected }: Props) => {
   return (
     <>
       <div
-        className={`flex justify-center p-[0.15rem] w-5 h-5 rounded-full ring-2 ring-gray-200 mr-1`}
+        className="flex justify-center p-[0.15rem] mr-1 w-5 h-5 rounded-full ring-2 ring-gray-200"
         onClick={handleSelected}
       >
         <button
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -9,4 +9,3 @@ export const Index: VFC = () => (
     <PostPanel />
   </>
 );
-
