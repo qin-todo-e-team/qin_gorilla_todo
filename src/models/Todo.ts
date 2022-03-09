9160641ded4c9c65611307f4d4f98d18d0a7a269
keeeei13c <kei.121003f@icloud.com>
2022-03-09 09:28:24 +0900

    型追加

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -6,6 +6,18 @@ export type Todo = {
   isDeleted: boolean;
 };
 
+export type TodoListType = {
+  todoId: string;
+  todoData: Todo;
+};
+
+export type TodoUpdateType = {
+  name?: string;
+  expire?: Date;
+  isFinished?: boolean;
+  isDeleted?: boolean;
+};
+
 export const TodoDefault = {
   name: "未入力",
   isFinished: false,
