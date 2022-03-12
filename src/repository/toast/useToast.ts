86cf7010b6912b3e99d7ed65d091a6fca4c99ff2
cti1650 <cti1650@gmail.com>
2022-03-12 23:46:29 +0900

    feat: スナックバーの出現位置変更 #71

--- src/repository/toast/useToast.ts
+++ src/repository/toast/useToast.ts
@@ -9,13 +9,13 @@ export const useToast = (): ToastType => {
   const successToast = (message: string) => {
     toast.success(message ?? "設定を変更しました", {
       duration: 1000,
-      position: "bottom-center",
+      position: "top-center",
     });
   };
   const errorToast = (message: string) => {
     toast.error(message ?? "変更に失敗しました", {
       duration: 1000,
-      position: "bottom-center",
+      position: "top-center",
     });
   };
   return {
