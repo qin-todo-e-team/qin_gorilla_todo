e851b85d9f16170b33ea59618a74e6d444187e30
cti1650 <cti1650@gmail.com>
2022-03-30 20:55:04 +0900

    fix: TodoFilter (#77)

--- src/lib/TodoFilter.ts
+++ src/lib/TodoFilter.ts
@@ -7,6 +7,12 @@ export const TodoFilter = (
   if (!todoList) {
     return [];
   }
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  tomorrow.setHours(0, 0, 0, 0);
+  const dayAfterTomorrow = new Date();
+  dayAfterTomorrow.setDate(dayAfterTomorrow.getDate() + 2);
+  dayAfterTomorrow.setHours(0, 0, 0, 0);
 
   return todoList.filter((todo) => {
     const date = todo.todoData?.expire.toDate();
@@ -15,11 +21,14 @@ export const TodoFilter = (
     }
     switch (title) {
       case "today":
-        return date.getDate() <= new Date().getDate();
+        return date.getTime() < tomorrow.getTime();
       case "tomorrow":
-        return date.getDate() === new Date().getDate() + 1;
+        return (
+          tomorrow.getTime() <= date.getTime() &&
+          date.getTime() < dayAfterTomorrow.getTime()
+        );
       case "upcoming":
-        return date.getDate() >= new Date().getDate() + 2;
+        return date.getTime() >= dayAfterTomorrow.getTime();
       default:
         return false;
     }
